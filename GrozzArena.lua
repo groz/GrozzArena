7ddcfafc456244b9839040a42050784bb73f9c7f
Tagir Magomedov <tagir.magomedov@gmail.com>
2013-07-15 00:07:00 -0700

    Hooked up replacement of arena target on click

--- GrozzArena.lua
+++ GrozzArena.lua
@@ -1,43 +1,57 @@
+local GrozzArena = { }
+GrozzArena.eventHandler = CreateFrame("Frame")
+GrozzArena.eventHandler.events = { }
+
 ----------------------------------------------------------------------------------------------------------
 -- MAIN
 ----------------------------------------------------------------------------------------------------------
+function GrozzArena.UpdateArenaTargetInMacro(macroName, arenaTarget)
+	local macroIndex = GetMacroIndexByName(macroName)
+	local macroBody = GetMacroBody(macroIndex)
+	local newMacroBody = macroBody:gsub("arena(%d+)", arenaTarget)
+	
+	EditMacro(macroIndex, macroName, nil, newMacroBody)
+	
+	print("Macro "..macroName.." retargeted to "..arenaTarget)
+end
 
-local function ArenaFrameClickHandler(self, button)
+function GrozzArena.ArenaFrameClickHandler(self, button)
+	print(self:GetName() .. " clicked by " .. button)
+
+	local frameName = "arena1"
+	local macroName = "myMacro1"
+	
 	if button == "RightButton" then
-		print("RIGHT BUTTON")
-	else	
-		print(self:GetName() .. " clicked with " .. button)
+		GrozzArena.UpdateArenaTargetInMacro(macroName, "arena1")
 	end
 end
 
-local function SetArenaHooks()
-	for i = 1,5 do
-		local arenaFrameName = "ArenaEnemyFrame"..i
+function GrozzArena.SetArenaHooks()
+	--for i = 1,5 do
+		local arenaFrameName = "PlayerFrame" --"ArenaEnemyFrame"..i
 		local arenaFrame = _G[arenaFrameName]
 
 		if (arenaFrame ~= nil) then
-			print("Hooking "..arenaFrameName)						
-			arenaFrame:HookScript("OnClick", ArenaFrameClickHandler)
+			print("Hooking "..arenaFrameName)
+			arenaFrame:HookScript("OnClick", GrozzArena.ArenaFrameClickHandler)
 		else
 			print(arenaFrameName .. " not found")
 		end
 		
-	end
+	--end
 end
 
 ----------------------------------------------------------------------------------------------------------
 -- HOOKING EVENTS
 ----------------------------------------------------------------------------------------------------------
-local eventHandler = CreateFrame("Frame")
 
-eventHandler:RegisterEvent("PLAYER_LOGIN")
-eventHandler:RegisterEvent("ARENA_PREP_OPPONENT_SPECIALIZATIONS")
+GrozzArena.eventHandler:RegisterEvent("PLAYER_LOGIN")
+GrozzArena.eventHandler:RegisterEvent("ARENA_PREP_OPPONENT_SPECIALIZATIONS")
 
-eventHandler:SetScript("OnEvent", function(self, event, ...)
+GrozzArena.eventHandler:SetScript("OnEvent", function(self, event, ...)
 	print(event)
 	
 	--if (event == "ARENA_PREP_OPPONENT_SPECIALIZATIONS") then
-		SetArenaHooks()
+		GrozzArena.SetArenaHooks()
 	--end
 end)
-
